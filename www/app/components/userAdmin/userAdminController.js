e01f2c7c85c7cc1017082be65bf83c311d782ded
Samuel Sutherland <samuelasutherland@gmail.com>
2018-02-17 15:45:12 +0000

    Adding creation on userAdmin

--- www/app/components/userAdmin/userAdminController.js
+++ www/app/components/userAdmin/userAdminController.js
@@ -2,8 +2,9 @@
   'use strict';
 
   angular.module('agora')
-    .controller('userAdminController', function($scope, GroupUserList, User, Group){
-      $scope.groups = Group.query()//+[{GroupName: "Show All"}]
+    .controller('userAdminController', function($scope, GroupUserList, GroupTypeGroupList, User, Group, GroupType){
+      $scope.groups = Group.query()
+      $scope.groupTypes = GroupType.query()
       // TODO: remove this
       $scope.user = {
         id: 125,
@@ -11,12 +12,34 @@
       };
       // end remove
       $scope.users = User.query();
-      $scope.$watch('groupFilter', function(value) {
+      $scope.$watch('typeFilter', applyTypeFilter);
+      $scope.$watch('groupFilter', applyGroupFilter);
+      var applyGroupFilter = function(value) {
         if (value) {
           $scope.users = GroupUserList.query({id: value.GroupID});
         } else {
           $scope.users = User.query();
         }
-      });
+      }
+      var applyTypeFilter = function(value) {
+        if (value) {
+          $scope.groups = GroupTypeGroupList.query({id: value.GroupTypeID});
+        } else {
+          $scope.groups = Group.query();
+          $scope.users = User.query();
+        }
+      }
+      $scope.newUser = new User();
+      // TODO: make this use the factory
+      $scope.addUser = function(){
+        console.log("Submitting")
+        console.log($scope.newUser)
+        User.save($scope.newUser, function(){
+          delete $scope.newUser;
+          $scope.newUser = new User();
+          applyTypeFilter($scope.typeFilter);
+          applyGroupFilter($scope.groupFilter);
+        });
+      };
     })
 })();
