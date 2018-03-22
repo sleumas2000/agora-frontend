b1764024baff6f6869b523126638a05eb6e62689
Samuel Sutherland <samuelasutherland@gmail.com>
2018-03-22 12:11:32 +0000

    changing isGoing behaviour

--- www/app/components/vote/votingSystems/welcomeController.js
+++ www/app/components/vote/votingSystems/welcomeController.js
@@ -4,11 +4,9 @@
   angular.module('agora')
     .controller('welcomeController', function($scope, $rootScope, User, Election){
       $scope.$on('systemsLoaded', function() {
-        console.log("going")
-        console.log($rootScope.election.systems[0])
         $rootScope.currentPage=-1
         $scope.nextPage=$rootScope.election.systems[$rootScope.currentPage+1]
-        $rootScope.isGoing = true
+        $rootScope.getGoing = function() {$rootScope.isGoing = true}
       })
     })
 })();
